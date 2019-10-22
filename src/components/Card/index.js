81ba0e461300ae88e61af912484f8909efbef589
Leandro Paranhos <leandro-paranhos@hotmail.com>
2019-10-22 16:01:51 -0300

    Estilizando os drags

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
 
 import { Container, Label } from './styles';
 
 export default function Card({ data }) {
+  const [{ isDragging }, dragRef] = useDrag({
+    item: {type: 'CARD'},
+    //Pega informações do item, como por exemplo,
+    //se está sendo arrastado ou não
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    })
+  })
+
+
   return (
-    <Container>
+    <Container isDragging={isDragging} ref={dragRef}>
       <header>
         {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
       <p>{data.content}</p>
-      {data.user && <img src={data.user} alt="" />}    
+      {data.user && <img src={data.user} alt="" />}
     </Container>
   );
 }
